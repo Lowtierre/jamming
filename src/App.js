eda7e9768de3cfb5259148b8bf86cf1c387c4eec
lowtierre <lorenzoterreni1997@gmail.com>
2023-08-27 11:19:52 +0200

    initialization repo jamming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const SONGS = [
+  {
+    id: 0,
+    title: 'Kill You',
+    author: 'Eminem'
+  },
+  {
+    id: 1,
+    title: 'The Real Slim Shady',
+    author: 'Eminem'
+  },
+  {
+    id: 2,
+    title: 'Without me',
+    author: 'Eminem'
+  }
+]
 
 function App() {
+
+  const [string, setString] = useState('');
+  const [tracklist, setTracklist] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+
+  const writingString = (string) => {
+    setString(string);
+  }
+
+  // const fetchSongs = (string) => {
+  // }
+
+  // const addSong = (song) => {
+  // }
+
+  // const removeSong = (song) => {
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>My App</header>
+      {/* <SearchBar /> */}
+      {/* <SearchResults /> */}
+      {/* <Playlist /> */}
     </div>
   );
 }
